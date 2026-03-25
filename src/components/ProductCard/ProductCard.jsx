8374c319de16b4ea110c36d5c78ce5d8a8a79df5
Adbrskk <aedobroskok@gmail.com>
2026-03-25 14:28:43 +0100

    add cartpage jsx

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,19 +1,37 @@
+import { useContext } from "react";
+import ProductsContext from "../../context";
 import "./ProductCard.css";
 
-function ProductCard({ image, name, price }) {
+function ProductCard({ product }) {
+  const { addToCart } = useContext(ProductsContext);
+
+  const handleAddToCart = () => {
+    addToCart(product);
+  };
+
   return (
     <article className="product-card">
-      <img src={image} alt={name} className="product-card__image" />
+      <img
+        src={product.image}
+        alt={product.name}
+        className="product-card__image"
+      />
 
-      <h3 className="product-card__title">{name}</h3>
+      <h3 className="product-card__title">{product.name}</h3>
 
       <div className="product-card__bottom">
         <div>
           <p className="product-card__label">ЦЕНА:</p>
-          <p className="product-card__price">{price} €</p>
+          <p className="product-card__price">{product.price} €</p>
         </div>
 
-        <button className="product-card__button">+</button>
+        <button
+          className="product-card__button"
+          onClick={handleAddToCart}
+          type="button"
+        >
+          +
+        </button>
       </div>
     </article>
   );
--- src/components/ProductsList/ProductsList.jsx
+++ src/components/ProductsList/ProductsList.jsx
@@ -1,48 +1,15 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useContext } from "react";
+import ProductsContext from "../../context";
 import ProductCard from "../ProductCard/ProductCard";
 import "./ProductsList.css";
 
-const BASE_URL = "https://69c39e43b780a9ba03e75a21.mockapi.io";
-
 function ProductsList() {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
-
-  const fetchProducts = async () => {
-    try {
-      const response = await axios.get(`${BASE_URL}/productsData`);
-      setProducts(response.data);
-    } catch (error) {
-      setError("Не удалось загрузить товары");
-      console.error(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchProducts();
-  }, []);
-
-  if (loading) {
-    return <p className="products-list__status">Загрузка...</p>;
-  }
-
-  if (error) {
-    return <p className="products-list__status">{error}</p>;
-  }
+  const { products } = useContext(ProductsContext);
 
   return (
     <div className="products-list">
       {products.map((product) => (
-        <ProductCard
-          key={product.id}
-          image={product.image}
-          name={product.name}
-          price={product.price}
-        />
+        <ProductCard key={product.id} product={product} />
       ))}
     </div>
   );
